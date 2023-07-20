38db95ab066260c2a354f5fd42a9c8a1794b4eab
namelew <diogomaciel.cunha@gmail.com>
2023-07-20 11:03:48 -0300

    Iniciando projeto

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Button extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <button>
+
+            </button>
+        )
+    }
+}
+
+export default Button;
