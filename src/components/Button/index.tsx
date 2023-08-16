3d83de820438f9e6f09e05f9d2ef40b425e74ac8
namelew <diogomaciel.cunha@gmail.com>
2023-08-16 11:35:31 -0300

    Adicioanando parâmetro className ao component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,13 @@ import style from "./Button.module.scss";
 interface Props {
     children?: React.ReactNode,
     type?: "button" | "submit" | "reset" | undefined,
+    className?: string,
     onClick?: () => void,
 }
 
-const Button = ({children, type = 'button', onClick} : Props) => {
+const Button = ({children, type = 'button', className, onClick} : Props) => {
     return (
-        <button onClick={onClick} type={type} className={style.button}>
+        <button onClick={onClick} type={type} className={className ? className : style.button}>
             {children}
         </button>
     );
